23b055fb32f3cfc3f1499868caec1f1ec2a75849
Ayush Kumar <ayushpanther@gmail.com>
2025-09-17 09:12:33 +0000

    addd thiss for help chatbot this is embeded code ;<script async type='mo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Chatbot from '@/components/chatbot/Chatbot';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Nivora : Home away from Home',
@@ -19,11 +19,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <Script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js' />
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
-        <Chatbot />
+        <zapier-interfaces-chatbot-embed is-popup='true' chatbot-id='cmf6zmt930024ddn3qbkai5f8'></zapier-interfaces-chatbot-embed>
       </body>
     </html>
   );
