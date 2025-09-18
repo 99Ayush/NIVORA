1d94d09c5b53ac4a0e275bccf57df1d8cb4e644b
Ayush Kumar <ayushpanther@gmail.com>
2025-09-18 15:33:55 +0000

    make the user database full proof so it can handle 50 realtime users

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
