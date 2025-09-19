c6f71df297c034cda621fd8411f3a859b3662094
Ayush Kumar <ayushpanther@gmail.com>
2025-09-19 10:21:56 +0000

    i feel like the website is responsing slow

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -90,14 +90,13 @@ export default {
         },
         'background-pan': {
           '0%': { backgroundPosition: '0% center' },
-          '50%': { backgroundPosition: '-100% center' },
-          '100%': { backgroundPosition: '0% center' },
+          '100%': { backgroundPosition: '-200% center' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'background-pan': 'background-pan 6s linear infinite',
+        'background-pan': 'background-pan 3s linear infinite',
       },
     },
   },
