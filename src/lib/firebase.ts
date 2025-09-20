b8ba245264ab82f8699483a241e5f63611d588da
Ayush Kumar <ayushpanther@gmail.com>
2025-09-20 18:51:15 +0000

    its showing error in enabling firebase services

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "studio-9356365406-11a53",
-  "appId": "1:495512062480:web:42968018a0ca9895",
-  "apiKey": "AIzaSyAVWzDHxakB1YkeaBVAhXOeje6JNo8Ob6s",
-  "authDomain": "studio-9356365406-11a53.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "495512062480"
+  "projectId": "nivora-449a7",
+  "appId": "1:267444331390:web:118e82b7b5c138096f9a0c",
+  "storageBucket": "nivora-449a7.appspot.com",
+  "apiKey": "AIzaSyC_t-YjV6A0f6Z_4p9B-T9x8r7Y0K-l2jE",
+  "authDomain": "nivora-449a7.firebaseapp.com",
+  "messagingSenderId": "267444331390"
 };
 
 // Initialize Firebase
