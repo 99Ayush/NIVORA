99e51927915d4f01a0df4c29c5237063a78a90a5
99Ayush <ayushpanther@gmail.com>
2025-09-21 12:21:44 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,19 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "nivora-449a7",
-  "appId": "1:267444331390:web:118e82b7b5c138096f9a0c",
-  "storageBucket": "nivora-449a7.appspot.com",
-  "apiKey": "AIzaSyC_t-YjV6A0f6Z_4p9B-T9x8r7Y0K-l2jE",
-  "authDomain": "nivora-449a7.firebaseapp.com",
-  "messagingSenderId": "267444331390"
+  apiKey: "AIzaSyAVWzDHxakB1YkeaBVAhXOeje6JNo8Ob6s",
+  authDomain: "studio-9356365406-11a53.firebaseapp.com",
+  projectId: "studio-9356365406-11a53",
+  storageBucket: "studio-9356365406-11a53.firebasestorage.app",
+  messagingSenderId: "495512062480",
+  appId: "1:495512062480:web:42968018a0ca982b9ad895",
+  measurementId: "G-8DDCW8WY5P"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
